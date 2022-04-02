99cf5fb28758c08f26dd3841c995a3510228557e
yudzeshka <yudzeshka@gmail.com>
2022-04-02 19:01:23 +0300

    added details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,3 +1,21 @@
+import { useSelector } from "react-redux";
+import { selectPeopleDetails } from "../redux/reducers/peopleDetails/selectors";
+
 export default function Details() {
-  return <div>Details page</div>;
+  const details = useSelector(selectPeopleDetails);
+
+  if (details.loading) {
+    return <div>Loading...</div>;
+  }
+
+  const { name, birth_year, skin_color, mass } = details.data;
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h4>{birth_year}</h4>
+      <p>{`Skin: ${skin_color}`}</p>
+      <p>{`mass: ${mass}`}</p>
+    </div>
+  );
 }
